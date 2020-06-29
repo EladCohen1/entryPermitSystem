198bf07b976b295356ead1a063dad9079ec8586a
EladCohen1 <eladc058@gmail.com>
2020-06-29 13:27:13 +0300

    feat(passowrd encryption)
    added encryption to password in db

--- server/users/users.controller.ts
+++ server/users/users.controller.ts
@@ -3,18 +3,31 @@ mongoose.connect("mongodb://localhost:27017/entryPermitDB", {
   useNewUrlParser: true,
 });
 import { userData, UserDataInterface } from "./users.model";
+const bcrypt = require("bcrypt");
 
 export class usersController {
-  public static async createUser(newUserData: UserDataInterface) {
-    var newUser = new userData(newUserData);
+  public static async createUser(name: string, passWord: string, id: number) {
+    var encryptedPass = await bcrypt.hash(passWord, 10);
+    var newUserObj: UserDataInterface = {
+      id: id,
+      userName: name,
+      passWord: encryptedPass,
+      userType: "guard",
+    };
+    var newUser = new userData(newUserObj);
     return newUser.save();
   }
 
   public static async authUser(userDataToAuth: UserDataInterface) {
-    return userData.find({
+    var user = await userData.find({
       userName: userDataToAuth.userName,
-      passWord: userDataToAuth.passWord,
     });
+    if (user[0]) {
+      if (await bcrypt.compare(userDataToAuth.passWord, user[0].passWord)) {
+        return user;
+      }
+    }
+    return false;
   }
 }
 
