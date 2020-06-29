d1871335daeb9d8ea3b71b0514d2c9c8c2252d43
EladCohen1 <eladc058@gmail.com>
2020-06-29 13:27:13 +0300

    feat(passowrd encryption)
    added encryption to password in db

--- server/users/users.router.ts
+++ server/users/users.router.ts
@@ -1,15 +1,19 @@
+import { UserDataInterface } from "./users.model";
+
 var express = require("express");
 var router = express.Router();
 var usersController = require("./users.controller");
 
-router.post("/", function (req: Request, res: any, next: any) {
-  usersController.createUser(req.body).then(function (data: any) {
-    res.json(data);
-  });
+router.post("/", function (req: any, res: any, next: any) {
+  usersController
+    .createUser(req.body.userName, req.body.passWord, req.body.id)
+    .then(function (data: any) {
+      res.json(data);
+    });
 });
 
-router.post("/auth", function (req: Request, res: any, next: any) {
-  usersController.authUser(req.body).then(function (data: any) {
+router.post("/auth", function (req: any, res: any, next: any) {
+  usersController.authUser(req.body).then((data: any) => {
     res.json(data);
   });
 });
