fd68641f7a495ec3c0dc1026f5102dac391ef2f4
EladCohen1 <eladc058@gmail.com>
2020-07-02 13:42:02 +0300

    feat(add non users entry permit form)
    add non user entry permit form and an animation of that form sliding into view

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -4,6 +4,9 @@ import { MatCardModule } from '@angular/material/card';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { MatStepperModule } from '@angular/material/stepper';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [],
@@ -13,12 +16,18 @@ import { MatStepperModule } from '@angular/material/stepper';
     ReactiveFormsModule,
     HttpClientModule,
     MatStepperModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
   ],
   exports: [
     MatCardModule,
     ReactiveFormsModule,
     HttpClientModule,
     MatStepperModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
   ],
 })
 export class CoreModule {
--- client/src/app/non-users/entry-permit-form/entry-permit-form.component.ts
+++ client/src/app/non-users/entry-permit-form/entry-permit-form.component.ts
@@ -1,13 +1,49 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from 'src/app/core/services/login.service';
+import { Validators, FormGroup, FormControl } from '@angular/forms';
+import { trigger, transition, style, animate } from '@angular/animations';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-entry-permit-form',
   templateUrl: './entry-permit-form.component.html',
   styleUrls: ['./entry-permit-form.component.scss'],
+  animations: [
+    trigger('slide', [
+      transition(':enter', [
+        style({ transform: 'translateX(100%)' }),
+        animate('500ms'),
+      ]),
+      transition(':leave', [
+        animate('500ms', style({ transform: 'translateX(100%)' })),
+      ]),
+    ]),
+  ],
 })
 export class EntryPermitFormComponent implements OnInit {
-  constructor() {}
+  showEntryPermitForm: boolean = true;
+  isLinear: boolean = true;
+  guestData: FormGroup = new FormGroup({
+    name: new FormControl('', Validators.required),
+    id: new FormControl('', Validators.required),
+  });
+  otherData: FormGroup = new FormGroup({
+    area: new FormControl('', Validators.required),
+  });
+  checkRequestForm: FormGroup = new FormGroup({
+    area: new FormControl('', Validators.required),
+  });
 
-  ngOnInit(): void {}
+  constructor(private router: Router) {}
+
+  outClick(path) {
+    this.showEntryPermitForm = false;
+    if (path !== '') {
+      setTimeout(() => {
+        this.router.navigate([path]);
+      }, 500);
+    }
+  }
+
+  ngOnInit() {}
 }
