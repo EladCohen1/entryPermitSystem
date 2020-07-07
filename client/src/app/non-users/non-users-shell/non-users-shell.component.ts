e58c532c9a8a8214e9b1d58a6d99c82a0f785fff
EladCohen1 <eladc058@gmail.com>
2020-07-07 14:58:15 +0300

    refactor(add shell component to non-users module)
    add shell component to non-users module to controll module startup

---
+++ client/src/app/non-users/non-users-shell/non-users-shell.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-non-users-shell',
+  templateUrl: './non-users-shell.component.html',
+  styleUrls: ['./non-users-shell.component.scss']
+})
+export class NonUsersShellComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- client/src/app/non-users/non-users.module.ts
+++ client/src/app/non-users/non-users.module.ts
@@ -4,9 +4,10 @@ import { EntryPermitFormComponent } from './entry-permit-form/entry-permit-form.
 import { RegisterComponent } from './register/register.component';
 import { nonUsersRoutingModule } from './non-users-routing.module';
 import { CoreModule } from '../core/core.module';
+import { NonUsersShellComponent } from './non-users-shell/non-users-shell.component';
 
 @NgModule({
-  declarations: [EntryPermitFormComponent, RegisterComponent],
+  declarations: [EntryPermitFormComponent, RegisterComponent, NonUsersShellComponent],
   imports: [CommonModule, nonUsersRoutingModule, CoreModule.forRoot()],
 })
 export class NonUsersModule {}
